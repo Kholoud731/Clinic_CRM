8a2aa7f5a092e86c0d97b2ab21925bc760ffe13a
Kholoud731 <>
2022-03-09 20:26:44 +0200

    adding the default logic for sending an email for the user

--- jobs/jobs.py
+++ jobs/jobs.py
@@ -19,41 +19,27 @@ def email_notification():
         user = User.objects.get(username =app.created_by )
 
         if date.day == (app_date.day + 1) and (date.hour - app_date.hour) == 0:
-            pass
+            send_mail(
+            subject="Your appointment",
+            message=f"Hello {app.created_by} is tomorrow",
+            from_email="kholoud.talaat93@gmail.com",
+            recipient_list=[f'{user.email}'],
+            fail_silently=False)
+
         if date.day == app_date.day and (date.hour - app_date.hour) == 6:
-            pass
+            send_mail(
+            subject="Your appointment in 6 hours",
+            message=f"Hello {app.created_by} Your appointment in 6 hours",
+            from_email="kholoud.talaat93@gmail.com",
+            recipient_list=[f'{user.email}'],
+            fail_silently=False)
+
         if date.day == app_date.day and (date.hour - app_date.hour) == 0:
-            pass
+            send_mail(
+            subject="Your appointment is now",
+            message=f"Hello {app.created_by} Your appointment is now",
+            from_email="kholoud.talaat93@gmail.com",
+            recipient_list=[f'{user.email}'],
+            fail_silently=False)
 
 
-
-        # for app in appointments.iterator():
-        #     print(app.status)
-        # user = User.objects.get(username =app.created_by )
-        # print(user.email)
-        # return 1
-                # print(app.created_by)
-#         user = User.objects.get(username =app.created_by )
-#         print(user.email)
-#         print(appointments['appointment_time'])
-#         if appointments['appointment_time'].hour - date.hour == 23:
-#             send_mail(
-#             subject="Your appointment",
-#             message=f"Hello {app.created_by} is tomorrow",
-#             from_email="kholoud.talaat93@gmail.com",
-#             recipient_list=[f'{user.email}'],
-#             fail_silently=False)
-#         elif appointments['appointment_time'].hour - date.hour == 6:
-#             send_mail(
-#             subject="Your appointment in 6 hours",
-#             message=f"Hello {app.created_by} Your appointment in 6 hours",
-#             from_email="kholoud.talaat93@gmail.com",
-#             recipient_list=[f'{user.email}'],
-#             fail_silently=False)
-#         elif appointments['appointment_time'].hour - date.hour == 0:
-#             send_mail(
-#             subject="Your appointment is now",
-#             message=f"Hello {app.created_by} Your appointment is now",
-#             from_email="kholoud.talaat93@gmail.com",
-#             recipient_list=[f'{user.email}'],
-#             fail_silently=False)
